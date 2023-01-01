da52fbb28543774cb90e194f1828f30c3265b699
Your Name <you@example.com>
2023-01-01 16:23:38 +0530

    Initial commit

--- lib/list.dart
+++ lib/list.dart
@@ -1,43 +1,60 @@
 import 'package:flutter/material.dart';
 
+void main(){
+  runApp(const MaterialApp(home:ListPage()));
+
+}
 class ListPage extends StatelessWidget{
   const ListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      appBar: AppBar(title: const Text("MODELS",style: TextStyle(fontStyle: FontStyle.italic),)),
+      appBar: AppBar(title: const Text("CONTACTS",style: TextStyle(fontStyle: FontStyle.normal),)),
       body: ListView(
-        children: const [
+        children:  [
           Card(
             color: Colors.greenAccent,
-            child: ListTile(title: Text("CLASSIC 350",style: TextStyle(fontStyle: FontStyle.italic)),
-              subtitle: Text("view spec"),
-              leading: CircleAvatar(backgroundImage: NetworkImage("https://imgd.aeplcdn.com/360x202/bw/models/royal-enfield-classic-350-single-channel-abs--bs-vi20200303121804.jpg?q=75")),
-              trailing: Icon(Icons.phone),),
+            child: ListTile(title: Text("DOMINIC",),
+
+              // subtitle: Text("view spec"),
+              // leading: CircleAvatar(backgroundImage: NetworkImage("https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/LetterD.svg/800px-LetterD.svg.png")),
+              trailing: Wrap(children: const [
+                Icon(Icons.phone),Icon(Icons.message)
+              ],),
+
+            ),
           ),
-          
-          ListTile(title: Text("STANDARD 350"),
-            subtitle: Text("view spec"),
-            leading: Image(image: NetworkImage("https://images.hindustantimes.com/auto/auto-images/bikes/royalenfield/bullet350/exterior_royal-enfield-bullet350_right-side-view_600x400-2.jpg")),
-            trailing: Icon(Icons.phone),),
-          
-          
-          ListTile(title: Text("METEOR 350"),
-            subtitle: Text("view spec"),
-            leading:Image(image: NetworkImage("https://imgd.aeplcdn.com/1200x900/bw/models/royal-enfield-meteor-350-fireball20201106124644.jpg")),
-            trailing: Icon(Icons.phone),),
-          
-          ListTile(title: Text("HIMALAYAN"),
-            subtitle: Text("view spec"),
-            leading:  Image(image: NetworkImage("https://cdni.autocarindia.com/Utils/ImageResizer.ashx?n=https://cdni.autocarindia.com/ExtraImages/20180112051043_RE-Himalayan-Sleet-Side-1.jpg&w=700&q=90&c=1")),
-            trailing: Icon(Icons.phone),),
-          
-          
-          ListTile(title: Text("CONTINENTAL GT"),
-            subtitle: Text("view spec"),
-            leading: Image(image: NetworkImage("https://media.zigcdn.com/media/model/2021/Mar/right-side-view-1796007878_930x620.jpg")),
-            trailing: Icon(Icons.phone),),
+
+           ListTile(title: Text("AARON"),
+            // subtitle: Text("view spec"),
+            // leading: Image(image: NetworkImage("https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Letter_A.svg/2048px-Letter_A.svg.png")),
+            trailing: Wrap(children: [
+              Icon(Icons.phone),Icon(Icons.message)
+            ],),),
+
+
+           ListTile(title: Text("CARL MAX"),
+            // subtitle: Text("view spec"),
+            // leading:Image(image: NetworkImage("https://w7.pngwing.com/pngs/554/796/png-transparent-letter-c-alphabet-letter-c-miscellaneous-text-monochrome.png")),
+            trailing:Wrap(children: [
+              Icon(Icons.phone),Icon(Icons.message)
+            ],),),
+
+           ListTile(title: Text("TEENA"),
+            // subtitle: Text("view spec"),
+            // leading:  Image(image: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRj1UK16iNnfr3DdqtEMiR8X1xw7bGoYV0v_Q&usqp=CAU")),
+            trailing:Wrap(children: [
+              Icon(Icons.phone),Icon(Icons.message)
+            ],),),
+
+
+           ListTile(title: Text("LOVE"),
+            // subtitle: Text("view spec"),
+            // leading: Image(image: NetworkImage("https://pngimg.com/uploads/letter_L/letter_L_PNG52.png")),
+            trailing:Wrap(children: [
+              Icon(Icons.phone),Icon(Icons.message)
+            ],),),
         ],
       ),
     );
